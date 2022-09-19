6b20e7efd27850aee3e636ae3a94156ed351f2e2
Andyequisde <andyrevollopro@gmail.com>
2022-09-19 15:40:06 -0300

    xd

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.set('views','./views')
+app.set('view engine', 'ejs') 
+const port = 3000
+//-----------------------------------------------------
+app.get('/', (pedido, respuesta) => {
+    respuesta.render('index')
+})
+
+//----------------------------------------------
+app.listen(port)
